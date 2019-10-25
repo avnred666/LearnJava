16f8c650c1430d7b96a66f751f6e2aabd70a63ea
Anoop Namboodiri <anoop.namboodiri@oracle.com>
2019-10-25 15:14:37 +0530

    FirstJavaAssignment

---
+++ src/Basics/Main.java
@@ -0,0 +1,9 @@
+package Basics;
+
+public class Main {
+    public static void main(String args[]) {
+        TestObject myDetails = new TestObject();
+        myDetails.printVar();
+        myDetails.printName("Anoop");
+    }
+}
---
+++ src/Basics/TestObject.java
@@ -0,0 +1,13 @@
+package Basics;
+
+public class TestObject {
+    String title = "This class prints name";
+
+    public void printName(String name) {
+        System.out.println(name);
+    }
+
+    public void printVar() {
+        System.out.println(title);
+    }
+}
